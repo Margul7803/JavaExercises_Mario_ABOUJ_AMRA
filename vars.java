422fbbee4deab3337cc163d930da181ca9c9d196
mario aboujamra <zgul7803@gmail.com>
2023-10-02 09:47:14 +0200

    first commit

---
+++ vars.java
@@ -0,0 +1,9 @@
+public class vars {
+    public static void main(String[] args){
+        Integer i = 0;
+        String s = "texte";
+        Double d = 1.1;
+
+        System.out.print(d);
+    }
+}
